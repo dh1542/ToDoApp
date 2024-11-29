6d50014fc27ba86fb7ee501289a7b8ec3a0e6d28
Dominik Hoffmann <dominik.hoffmann@iso-gruppe.com>
2024-11-29 13:10:18 +0100

    Initalized spring

---
+++ FinanceTrackerServer/src/test/java/tech/dh/financetrackerserver/FinanceTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dh.financetrackerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
