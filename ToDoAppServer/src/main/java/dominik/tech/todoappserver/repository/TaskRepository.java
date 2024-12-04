82145247e7566fff98e497ecbb13b7a138185d95
Dominik Hoffmann <dominik151099@outlook.de>
2024-12-04 11:05:35 +0100

    #4: Introduced Task DTO

--- ToDoAppServer/src/main/java/dominik/tech/todoappserver/repository/TaskRepository.java
+++ ToDoAppServer/src/main/java/dominik/tech/todoappserver/repository/TaskRepository.java
@@ -1,12 +1,14 @@
 package dominik.tech.todoappserver.repository;
 
-
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import dominik.tech.todoappserver.entity.Task;
 
 
 @Repository
-public class TaskRepository {
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    saveTask(Ta)
+
 
 
 }
