210005fdeb72488ed66f64feda280fa9a624448c
Dominik Hoffmann <dominik151099@outlook.de>
2024-12-04 12:05:30 +0100

    #4: Fixed creation of tasks and refactored health check

--- ToDoAppServer/src/main/java/dominik/tech/todoappserver/controller/TaskController.java
+++ ToDoAppServer/src/main/java/dominik/tech/todoappserver/controller/TaskController.java
@@ -1,11 +1,12 @@
 package dominik.tech.todoappserver.controller;
 
+import dominik.tech.todoappserver.entity.Task;
 import dominik.tech.todoappserver.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/task")
@@ -14,18 +15,17 @@ public class TaskController {
     @Autowired
     TaskService taskService = new TaskService();
 
-    @GetMapping("/health")
-    @ResponseBody
-    public String healthCheck() {
-        System.out.printf("Health check\n");
-        return "OK";
-    }
 
-    @GetMapping("/addTask")
+    @PostMapping("/addTask")
     @ResponseBody
-    public String addTask() {
-        taskService.createTask("Task 1", "Description 1");
-        return "Task added";
+    public ResponseEntity<String> addTask(@RequestHeader("name") String name, @RequestHeader ("description") String description) {
+        try{
+            Task createdTask = taskService.createTask(name, description);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Created task with id: " + createdTask.getId());
+        } catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to create task");
+        }
+
     }
 
 }
