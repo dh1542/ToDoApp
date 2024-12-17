95fd5812192df5579f75a20efe59f023817fe462
Dominik Hoffmann <dominik.hoffmann@iso-gruppe.com>
2024-12-17 11:39:05 +0100

    Created model to dynamically fetch changes of tasks

--- ToDoApp-UI/src/models/taskModel.ts
+++ ToDoApp-UI/src/models/taskModel.ts
@@ -1,11 +1,37 @@
-import { ref } from "vue";
+import { reactive } from "vue";
+import { Task } from "@/types/Task";
+import { inject } from "vue";
 
-export default function useTask() {
-  const tasks = ref([]);
+export default class TaskModel {
+  private serverUrl = inject<string>("serverUrl");
+  private allTaskEnpointUrl = "/api/tasks/all";
 
-  const fetchTasks = async () => {
-    const response = await fetch("http://localhost:8080/tasks");
-    tasks.value = await response.json();
-  };
-  return { tasks, fetchTasks };
+  constructor() {
+    this.getAllTasks();
+  }
+  
+  public state = reactive<{tasks: Task[]}>({
+    tasks: [],
+  })
+  
+  async getAllTasks(): Promise<Task[]> {
+    try{
+      const response = await fetch(this.serverUrl + this.allTaskEnpointUrl);
+      if(!response.ok){
+        throw new Error("Error fetching tasks");
+      }
+      const data: Task[] = await response.json();
+      this.state.tasks = data;
+      return data;
+    }
+    catch(error){
+      console.error("Error fetching tasks", error);
+      return [];
+    }
+  }
+
+  async refreshTasks(): Promise<Task[]> {
+    return this.getAllTasks();
+  }
 }
+
